d143e5842ee1c1e6fec66b81a80c5f2e0865b268
loc <lekimtanloc2002@gmail.com>
2022-11-26 16:43:28 +0700

    Add project files.

---
+++ Attendance/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Attendance
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.tb_usn = new System.Windows.Forms.TextBox();
+            this.tb_pwd = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(220, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(220, 181);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // btn_login
+            // 
+            this.btn_login.Location = new System.Drawing.Point(363, 260);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(94, 29);
+            this.btn_login.TabIndex = 2;
+            this.btn_login.Text = "Login";
+            this.btn_login.UseVisualStyleBackColor = true;
+            // 
+            // tb_usn
+            // 
+            this.tb_usn.Location = new System.Drawing.Point(319, 123);
+            this.tb_usn.Name = "tb_usn";
+            this.tb_usn.Size = new System.Drawing.Size(179, 27);
+            this.tb_usn.TabIndex = 3;
+            this.tb_usn.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // tb_pwd
+            // 
+            this.tb_pwd.Location = new System.Drawing.Point(319, 181);
+            this.tb_pwd.Name = "tb_pwd";
+            this.tb_pwd.Size = new System.Drawing.Size(179, 27);
+            this.tb_pwd.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(331, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(151, 67);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Login";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(769, 401);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tb_pwd);
+            this.Controls.Add(this.tb_usn);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Button btn_login;
+        private TextBox tb_usn;
+        private TextBox tb_pwd;
+        private Label label3;
+    }
+}
